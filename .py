f354e349bad7f02991d933da9b0a3a2bcb3779ea
chandrakalahs <115870285+chandrakalahs@users.noreply.github.com>
2024-08-26 11:23:55 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,8 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python311/sanjana.py ==
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python311/sanjana.py", line 4, in <module>
+    car==['AUDI','BMW','FORD','TESLA','JAgUAR','MERCEDES']
+NameError: name 'car' is not defined. Did you mean: 'chr'?
